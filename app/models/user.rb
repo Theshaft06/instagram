f76fc6109c37a846ab8b24048597a6d9de49ff02
Matt Tucker <mtuck06@gmail.com>
2018-06-04 15:04:09 -0500

    current working version 1

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	# All the users that I am following
+	has_many :follow_following, foreign_key: :follower_id, class_name: 'Follow'
+	has_many :following, through: :follow_following, source: :followee
+
+	# All the users that are following me
+	has_many :follow_followers, foreign_key: :followee_id, class_name: 'Follow'
+	has_many :followers, through: :follow_followers, source: :follower
+end
+
